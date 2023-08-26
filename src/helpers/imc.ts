e1edfbbbcfa9c19cf39846c0ea6b9559f78c0086
gabrielli-lima <gabriellilima165@gmail.com>
2023-08-26 16:52:10 -0300

    feat: add RightSide

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -7,9 +7,9 @@ export type Level = {
 }
 export const levels: Level[] = [
     { title: 'Magreza', color: '#96A3AB', icon: 'down', imc: [0, 18.5]},
-    { title: 'Norma', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9]},
-    { title: 'Norma', color: '#E2B039', icon: 'down', imc: [25, 30]},
-    { title: 'Norma', color: '#C3423F', icon: 'down', imc: [30.1, 99]},
+    { title: 'Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9]},
+    { title: 'Sobrepeso', color: '#E2B039', icon: 'down', imc: [25, 30]},
+    { title: 'Obesidade', color: '#C3423F', icon: 'down', imc: [30.1, 99]},
 ];
 
 export const calculateImc = (height: number, weight: number) => {

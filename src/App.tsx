f11d35415e63524d63bdc533e3fa7c63c79a1532
gabrielli-lima <gabriellilima165@gmail.com>
2023-08-27 21:46:57 -0300

    refactor: modificando a logica do sistema

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
-import { levels, calculateImc} from './helpers/imc'
-import {GridItem} from './components/GridItem'
+import { levels, calculateImc, Level } from './helpers/imc'
+import { GridItem } from './components/GridItem'
 
 const App = () => {
-  const [heightField, setHeightField] = useState<number>(0)
-  const [weightField, setWeightField] = useState<number>(0)
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
 
-  const handleCalculateButton = () =>{
-    if(heightField && weightField){
-      alert("a")
-    }else{
-      alert("Digite todos os campos")
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField));
+    } else {
+      alert("Digite todos os campos");
     }
   }
 
@@ -33,7 +34,7 @@ const App = () => {
             value={heightField > 0 ? heightField : ''}
             onChange={e => setHeightField(parseFloat(e.target.value))}
           />
-           <input type="number"
+          <input type="number"
             placeholder="Digite o seu peso. Ex: 65.7 (em kg)"
             value={weightField > 0 ? weightField : ''}
             onChange={e => setWeightField(parseFloat(e.target.value))}
@@ -41,11 +42,20 @@ const App = () => {
           <button onClick={handleCalculateButton}>Calcular</button>
         </div>
         <div className={styles.rightSide}>
-          <div className={styles.grid}>
-            {levels.map((item, key)=>(
-              <GridItem key={key} item={item}/>
-            ))}
-          </div>
+          {!toShow &&
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} item={item} />
+              ))}
+
+            </div>
+          }{
+            toShow &&
+            <div className={styles.rightBig}>
+              <div className={styles.righArrow}></div>
+              <GridItem item={toShow}/>
+            </div>
+          }
         </div>
       </div>
     </div>

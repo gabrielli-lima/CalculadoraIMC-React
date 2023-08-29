0fd5d77a6f8dab01af4b5d67ef1f506b1c80bbd9
gabrielli-lima <gabriellilima165@gmail.com>
2023-08-28 22:14:31 -0300

    refactor: responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import styles from './App.module.css';
-import poweredImage from './assets/powered.png';
-import { levels, calculateImc, Level } from './helpers/imc';
-import { GridItem } from './components/GridItem';
-import leftArrowImage from './assets/leftarrow.png';
+import { useState } from "react";
+import styles from "./App.module.css";
+import poweredImage from "./assets/powered.png";
+import { levels, calculateImc, Level } from "./helpers/imc";
+import { GridItem } from "./components/GridItem";
+import leftArrowImage from "./assets/leftarrow.png";
 
 const App = () => {
   const [heightField, setHeightField] = useState<number>(0);
@@ -16,12 +16,12 @@ const App = () => {
     } else {
       alert("Digite todos os campos");
     }
-  }
-  const handleBackButton = ()=>{
+  };
+  const handleBackButton = () => {
     setToShow(null);
     setHeightField(0);
     setWeightField(0);
-  }
+  };
 
   return (
     <div className={styles.main}>
@@ -33,44 +33,54 @@ const App = () => {
       <div className={styles.container}>
         <div className={styles.leftSide}>
           <h1>Calcule o seu IMC</h1>
-          <p>IMC significa Índice de Massa Corporal, usado para calcular o peso ideal de cada pessoa. Esta é uma medida de referência internacional reconhecida pela Organização Mundial da Saúde</p>
+          <p>
+            IMC significa Índice de Massa Corporal, usado para calcular o peso
+            ideal de cada pessoa. Esta é uma medida de referência internacional
+            reconhecida pela Organização Mundial da Saúde
+          </p>
 
-          <input type="number"
+          <input
+            type="number"
             placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
-            value={heightField > 0 ? heightField : ''}
-            onChange={e => setHeightField(parseFloat(e.target.value))}
+            value={heightField > 0 ? heightField : ""}
+            onChange={(e) => setHeightField(parseFloat(e.target.value))}
             disabled={toShow ? true : false}
           />
-          <input type="number"
+          <input
+            type="number"
             placeholder="Digite o seu peso. Ex: 65.7 (em kg)"
-            value={weightField > 0 ? weightField : ''}
-            onChange={e => setWeightField(parseFloat(e.target.value))}
+            value={weightField > 0 ? weightField : ""}
+            onChange={(e) => setWeightField(parseFloat(e.target.value))}
             disabled={toShow ? true : false}
           />
-          <button onClick={handleCalculateButton} disabled={toShow ? true : false}>Calcular</button>
+          <button
+            onClick={handleCalculateButton}
+            disabled={toShow ? true : false}
+          >
+            Calcular
+          </button>
         </div>
         <div className={styles.rightSide}>
-          {!toShow &&
+          {!toShow && (
             <div className={styles.grid}>
               {levels.map((item, key) => (
                 <GridItem key={key} item={item} />
               ))}
-
             </div>
-          }{
-            toShow &&
+          )}
+          {toShow && (
             <div className={styles.rightBig}>
               <div className={styles.rightArrow} onClick={handleBackButton}>
                 <img src={leftArrowImage} alt="" width={25} />
               </div>
-              
-              <GridItem item={toShow}/>
+
+              <GridItem item={toShow} />
             </div>
-          }
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
